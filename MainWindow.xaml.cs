1a546d44c624b7268585fc6f717d776cdbaff732
Prém Ferenc <prem.ferenc@burg.lo.hu>
2024-09-06 10:41:46 +0200

    done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using LegoStorageFiles.Models;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace LegoStorageFiles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static ObservableCollection<LegoBricks> legoBrickList = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFromBSXFile()
+        {
+            OpenFileDialog openFileDialog = new();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    XDocument document = XDocument.Load(openFileDialog.FileName);
+                    foreach (var item in document.Descendants("Item"))
+                    {
+                        legoBrickList.Add(new(item.Element("ItemID").Value, item.Element("ItemName").Value, item.Element("CategoryName").Value, item.Element("ColorName").Value, Convert.ToInt32(item.Element("Qty").Value)));
+                    }
+
+                    legoInfoHolderDg.ItemsSource = legoBrickList;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("A kiválaszott elem rossz formátumú, vagy nem létezik");
+                }
+            }
+        }
+
+        private void LoadFile(object sender, RoutedEventArgs e)
+        {
+            LoadFromBSXFile();
+        }
+        
+        private void SearchByIdAndName(object sender, TextChangedEventArgs e)
+        {
+            if (searchByTxt.Text.Any(char.IsDigit))
+            {
+                legoInfoHolderDg.ItemsSource = legoBrickList.Where(x => x.ItemId.StartsWith(searchByTxt.Text));
+                
+            } else
+            {
+                legoInfoHolderDg.ItemsSource = legoBrickList.Where(x => x.ItemName.ToLower().StartsWith(searchByTxt.Text) || x.CategoryName.ToLower().StartsWith(searchByTxt.Text));
+            }
+
+            if (searchByTxt.Text == "")
+            {
+                legoInfoHolderDg.ItemsSource = legoBrickList;
+            }
+        }
+    }
+}
---
+++ Models/LegoBricks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoStorageFiles.Models
+{
+    internal class LegoBricks
+    {
+        string itemId;
+        string itemName;
+        string categoryName;
+        string colorName;
+        int quantity;
+
+        public LegoBricks(string itemId, string itemName, string categoryName, string colorName, int quantity)
+        {
+            this.itemId = itemId;
+            this.itemName = itemName;
+            this.categoryName = categoryName;
+            this.colorName = colorName;
+            this.quantity = quantity;
+        }
+
+
+        public string ItemId { get => itemId; set => itemId = value; }
+        public string ItemName { get => itemName; set => itemName = value; }
+        public string CategoryName { get => categoryName; set => categoryName = value; }
+        public string ColorName { get => colorName; set => colorName = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+    }
+}
